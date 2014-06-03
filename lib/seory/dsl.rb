fec6925accf23559335df5c7d6a51448d9432e8c
moro <moronatural@gmail.com>
2014-06-03 11:59:53 +0900

    Clean code

--- lib/seory/dsl.rb
+++ lib/seory/dsl.rb
@@ -2,29 +2,13 @@ require 'seory/page_contents'
 require 'seory/runtime'
 
 module Seory
-  class PageContentsBuilder
-    def initialize(*conditions)
-      @page_contents = PageContents.new(*conditions)
-    end
-
-    def build!(&block)
-      instance_exec(&block)
-
-      @page_contents
-    end
-
-    Seory::Runtime::CONTENTS.each do |name|
-      define_method(name) do |val = nil, &block|
-        @page_contents.define(name, val, &block)
-      end
-    end
-  end
-
   module Dsl
     extend self
 
     def describe(&block)
-      instance_exec(&block)
+      [].tap do |repositories|
+        Descriptor.new(repositories).instance_exec(&block)
+      end
     end
 
     def lookup(controller)
@@ -32,18 +16,36 @@ module Seory
       Seory::Runtime.new(page_contents, controller)
     end
 
-    def match(*conditions, &def_builder)
-      repositories << PageContentsBuilder.new(*conditions).build!(&def_builder)
+    class PageContentsBuilder
+      def initialize(*conditions)
+        @page_contents = PageContents.new(*conditions)
+      end
+
+      def build!(&block)
+        instance_exec(&block)
+
+        @page_contents
+      end
+
+      Seory::Runtime::CONTENTS.each do |name|
+        define_method(name) do |val = nil, &block|
+          @page_contents.define(name, val, &block)
+        end
+      end
     end
 
-    def default(&def_builder)
-      match(:default, &def_builder)
-    end
+    class Descriptor
+      def initialize(repositories)
+        @repositories = repositories
+      end
 
-    private
+      def match(*conditions, &def_builder)
+        @repositories << PageContentsBuilder.new(*conditions).build!(&def_builder)
+      end
 
-    def repositories
-      @repositories ||= []
+      def default(&def_builder)
+        match(:default, &def_builder)
+      end
     end
   end
 end
