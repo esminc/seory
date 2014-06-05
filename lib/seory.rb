788f1344230f01d79d51ff71701c65615be9f5ae
moro <moronatural@gmail.com>
2014-06-05 16:23:28 +0900

    Change content name image_url -> og_image_url

--- lib/seory.rb
+++ lib/seory.rb
@@ -1,7 +1,7 @@
 require "seory/version"
 
 module Seory
-  CONTENTS = %w[title h1 h2 meta_description meta_keywords canonical_url image_url].map(&:to_sym)
+  CONTENTS = %w[title h1 h2 meta_description meta_keywords canonical_url og_image_url].map(&:to_sym)
 
   class Error < RuntimeError
   end
