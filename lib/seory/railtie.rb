6bdd36ac4ed1cea0ef466184626d10d25dec0cd3
Yuka Kato <y-kato@esm.co.jp>
2015-10-25 13:39:44 +0900

    Include Seory::RailsHelper in Railtie

--- lib/seory/railtie.rb
+++ lib/seory/railtie.rb
@@ -6,6 +6,10 @@ module Seory
       config_dir = (Rails.root + Seory.config_dir)
 
       app.config.to_prepare { config_dir.each_child {|src| src.extname == '.rb' && load(src) } }
+
+      ActiveSupport.on_load :action_view do
+        include Seory::RailsHelper
+      end
     end
   end
 end
